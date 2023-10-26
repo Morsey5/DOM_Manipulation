001fd00af9e9c758c0cc17864bba521039cc239e
Morsey5 <temeikamorse@gmail.com>
2023-10-25 23:57:57 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+//import "./styles.css";
+
+// part one
+const mainEl = document.querySelector("main") ;
+mainEl.style.height = "100vh"
+const h1Element = document.createElement("h1")
+h1Element.textContent = "DOM Manipulation";
+
+// join the h1 to the content
+mainEl.appendChild(h1Element);
+h1Element.style.color = "white";
+mainEl.style.backgroundColor= "#4E4E4D";
+console.log("hellow world")
+mainEl.classList.add("flex-ctr");
+
+
+
+//part two
+const topMenuEl = document.querySelector("#top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "#0e9aa7";
+topMenuEl.classList.add("flex-around");
+
+
+//part three
+// Menu data structure
+const menuLinks = [
+    { text: 'about', href: '/about' },
+  { text: 'catalog', href: '#', subLinks: [
+    { text: 'all', href: '/catalog/all' },
+    { text: 'top selling', href: '/catalog/top' },
+    { text: 'search', href: '/catalog/search' },
+  ] },
+  { text: 'orders', href: '#', subLinks: [
+    { text: 'new', href: '/orders/new' },
+    { text: 'pending', href: '/orders/pending' },
+    { text: 'history', href: '/orders/history' },
+  ] },
+  { text: 'account', href: '#', subLinks: [
+    { text: 'profile', href: '/account/profile' },
+    { text: 'sign out', href: '/account/signout' },
+  ] },
+  ];
+
+  menuLinks.forEach(link => {
+    const linkElement = document.createElement("a");
+    linkElement.textContent = link.text;
+    linkElement.href = link.href;
+    
+    topMenuEl.appendChild(linkElement);
+  });
+
+
+  //Second Part
+//part three
+
+  const subMenuEl = document.querySelector("#sub-menu");
+  subMenuEl.style.height = "100%";
+  subMenuEl.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--sub-menu-bg');
+  subMenuEl.classList.add("flex-around");
+  subMenuEl.style.position = "absolute";
+  subMenuEl.style.top = "0";
+
+//part four
+
+  const topMenuLinks = topMenuEl.querySelectorAll("a");
+  topMenuEl.addEventListener("click", function(event) {
+    event.preventDefault();
+    if (!event.target.matches("a")) {
+        return;
+      }
+
+      console.log(event.target.textContent);
+    });
+
+
+
+    topMenuEl.addEventListener("click", function(event) {
+    
+        event.preventDefault();
+      
+        if (!event.target.matches("a")) {
+          return;
+        }
+
+        topMenuLinks.forEach(function(link) {
+          link.classList.remove("active");
+        });
+      
+        event.target.classList.toggle("active");
+      
+        console.log(event.target.textContent);
+      });
